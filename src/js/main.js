3ad52cb95d4061e3a036cdd8bd289f661162381d
Levi Schrumpf <levimatthan@gmail.com>
2016-12-02 02:30:10 -0500

    made a site

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-console.log('working');
+$(document).ready(function() {
+	//initial load
+	$("#content").load('content/home.html');
+	
+	//menu clicks and active class
+	$('#nav ul li a').click(function(e){
+		$('#nav ul li a').removeClass('active');
+		$(this).addClass('active');
+		var page = $(this).attr('href');
+		console.log(page);
+		$('#content').load('content/' + page + '.html');
+		return false;
+	});
+});
