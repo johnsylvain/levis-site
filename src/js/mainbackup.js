4ded5804e20c59cf6de6c8f9ed35507a68d1b4cf
Levi Schrumpf <levimatthan@gmail.com>
2017-05-16 23:53:27 -0400

    fixed contact form

--- src/js/mainbackup.js
+++ src/js/mainbackup.js
@@ -27,3 +27,24 @@ $(document).ready(function() {
 		return false;
 	});
 });
+
+
+
+
+
+	/*$('.nav li a').click(function(e){
+		$('a').removeClass('active');
+		$(this).addClass('active');
+		var page = $(this).attr('href');
+		console.log(page);
+		$('#content').load('content/' + page + '.html', function(){
+			$('.projectrow a, p a').click(function(e){
+				e.preventDefault();
+				console.log(page);
+				var page = $(this).attr('href');
+				console.log(page);
+				$('#content').load('content/' + page + '.html');
+			});
+		});
+		return false;
+	});*/
